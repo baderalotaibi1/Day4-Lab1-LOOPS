847a72ad5c233a0abfdd9c0643205141c0265904
bader <bader2007as@hotmail.com>
2022-08-03 11:26:00 +0300

    lab1 Day4 (Loops )

---
+++ lab3_day4.py
@@ -0,0 +1,19 @@
+for num in range(45,210):
+    if num==100:
+        continue
+    elif num ==205:
+        break
+    print(num)
+x=""
+while(x!=0):
+
+    x = input("what is the product of 7 * 24 ?"
+              "note enter 0 to exit ")
+    x = int(x)
+
+    if x==168:
+      print( "You answered this Question correctly")
+
+    else:
+      print("Your Answer is wrong try again.." )
+
